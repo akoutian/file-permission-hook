0cebb2c0dcf0f206ba45d5c668a5befb41313218
Anastasios Koutian <akoutian2@gmail.com>
2021-02-02 22:56:58 +0000

    Pass the filenames to the git wrapper

--- file_permission_hook/main.py
+++ file_permission_hook/main.py
@@ -4,8 +4,9 @@ import argparse
 from file_permission_hook.subprocess_wrapper import git_wrapper
 
 
-def check_files(paths):
-    git_wrapper()
+def check_files(args):
+    for path in args.filenames:
+        git_wrapper(path)
     exit(1)
 
 
@@ -13,10 +14,7 @@ def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("filenames", nargs="*")
 
-    args = parser.parse_args()
-    print(args)
-
-    return check_files(args)
+    return check_files(parser.parse_args())
 
 
 if __name__ == "__main__":
--- file_permission_hook/subprocess_wrapper.py
+++ file_permission_hook/subprocess_wrapper.py
@@ -1,6 +1,8 @@
 import subprocess
 
-def git_wrapper():
-    subp = subprocess.run(['git', 'status', '--porcelain=v2', '-z'], capture_output=True)
-    print(subp.stdout)
 
+def git_wrapper(path):
+    subp = subprocess.run(
+        ["git", "status", path, "--porcelain=v2", "-z"], capture_output=True
+    )
+    print(subp.stdout)
