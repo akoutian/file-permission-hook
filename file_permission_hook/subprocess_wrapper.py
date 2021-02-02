992cad9fe25a64a2201e389c2dbdb44fb1e149e1
Anastasios Koutian <akoutian2@gmail.com>
2021-02-02 23:07:50 +0000

    Call "subprocess.run" with utf-8 encoding
    So that I get a string instead of bytes at the output

--- file_permission_hook/subprocess_wrapper.py
+++ file_permission_hook/subprocess_wrapper.py
@@ -3,6 +3,6 @@ import subprocess
 
 def git_wrapper(path):
     subp = subprocess.run(
-        ["git", "status", path, "--porcelain=v2", "-z"], capture_output=True
+        ["git", "status", path, "--porcelain=v2", "-z"], capture_output=True, encoding="UTF-8"
     )
     print(subp.stdout)
