05375c45898c4650c75703dae122fc8db600d48e
Anastasios Koutian <akoutian2@gmail.com>
2021-02-03 20:29:06 +0000

    Add some typing stuff

--- file_permission_hook/main.py
+++ file_permission_hook/main.py
@@ -3,19 +3,21 @@ import argparse
 
 from file_permission_hook.subprocess_wrapper import git_wrapper
 
+from typing import Optional, Sequence, List
 
-def check_files(args):
-    for path in args.filenames:
+
+def check_files(paths: List[str]) -> int:
+    for path in paths:
         git_output = git_wrapper(path)
         print(git_output)
     exit(1)
 
 
-def main():
+def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("filenames", nargs="*")
 
-    return check_files(parser.parse_args())
+    return check_files(parser.parse_args().filenames)
 
 
 if __name__ == "__main__":
