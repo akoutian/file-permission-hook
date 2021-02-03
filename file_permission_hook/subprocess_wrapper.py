f12b743378da7210bb0bf6cb82eb1efd09afa6f1
Anastasios Koutian <akoutian2@gmail.com>
2021-02-03 00:45:51 +0000

    Black

--- file_permission_hook/subprocess_wrapper.py
+++ file_permission_hook/subprocess_wrapper.py
@@ -3,7 +3,8 @@ import subprocess
 
 def git_wrapper(path):
     subp = subprocess.run(
-        ["git", "status", path, "--porcelain=v2", "-z"], capture_output=True, encoding="UTF-8"
+        ["git", "status", path, "--porcelain=v2", "-z"],
+        capture_output=True,
+        encoding="UTF-8",
     )
     return subp.stdout
-
