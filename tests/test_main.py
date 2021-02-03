c362a84c824106464bebdb102b75bb5c7ef7e105
Anastasios Koutian <akoutian2@gmail.com>
2021-02-03 23:26:40 +0000

    Fix typo

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ from file_permission_hook import main
 
 
 class TestMain(unittest.TestCase):
-    def test_help(self):
+    def test_main(self):
         self.assertEqual(main.main(), 0)
 
 if __name__ == "__main__":
